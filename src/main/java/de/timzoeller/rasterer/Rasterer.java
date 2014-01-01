b7efb6e508e5378a31e3b5ffb6cfec6dbfd250c4
tmzll <mail@tim-zoeller.de>
2014-01-01 20:39:38 +0100

    Added Renderer as a parameter

--- src/main/java/de/timzoeller/rasterer/Rasterer.java
+++ src/main/java/de/timzoeller/rasterer/Rasterer.java
@@ -3,13 +3,13 @@ package de.timzoeller.rasterer;
 import de.timzoeller.rasterer.converter.ImageConverter;
 import de.timzoeller.rasterer.model.RasterImage;
 import java.awt.image.BufferedImage;
-import de.timzoeller.rasterer.renderer.OvalRasterRenderer;
+import de.timzoeller.rasterer.renderer.RasterRenderer;
 
 public class Rasterer {
 
-    public BufferedImage convertImageToRaster(BufferedImage source, int horizontalResolution, int pixelSize) {
+    public BufferedImage convertImageToRaster(BufferedImage source, int horizontalResolution, int pixelSize, RasterRenderer renderer) {
         RasterImage raster = buildRasterFromImage(source, horizontalResolution);
-        return renderRasterToImage(raster, pixelSize);        
+        return renderRasterToImage(raster, pixelSize, renderer);        
     }
 
     private RasterImage buildRasterFromImage(BufferedImage source, int horizontalResolution) {
@@ -17,8 +17,7 @@ public class Rasterer {
         return converter.imageToRaster(source, horizontalResolution);
     }
 
-    private BufferedImage renderRasterToImage(RasterImage raster, int pixelSize) {
-        OvalRasterRenderer renderer = new OvalRasterRenderer();
+    private BufferedImage renderRasterToImage(RasterImage raster, int pixelSize, RasterRenderer renderer) {
         return renderer.toBufferedImage(raster, pixelSize);
     }
 
